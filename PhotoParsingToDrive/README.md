2f44d319dcdbb6d90b8c3d8f9d50bf5fc117480c
umut <umutpolat1912@gmail.com>
2023-08-17 16:40:12 -0700

    Updated instructions and photo categories

--- PhotoParsingToDrive/README.md
+++ PhotoParsingToDrive/README.md
@@ -2,5 +2,12 @@
 This script is used to download all the images provided in the `data.xlsx` file and upload them to the Google Drive: https://drive.google.com/drive/u/1/folders/1nkHQ-7wis0yuvaXAn6i7axzYsToxaAbR
 
 Here are important reminders:
+- Don't forget to install Python3 and PIP from `https://www.python.org/downloads/`
 - Don't forget to `pip install pandas && pip install openpyxl && pip install pydrive`
-- Don't forget to setup `client_secrets.json` to be able to access Google Drive. Here are the instructions: https://pythonhosted.org/PyDrive/quickstart.html#authentication
+- Don't forget to setup `client_secrets.json` to be able to access Google Drive. Here are the instructions: https://pythonhosted.org/PyDrive/quickstart.html#authentication which you should do so with the admin GENARC-I email
+
+Step by step how to guide:
+- Step 1: After completing the reminders above, clone this repository by following the instructions on this link: https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository
+- Step 2: Open a terminal and navigate into `PhotoParsingToDrive` folder
+- Step 3: Run the script via `python3 main.py`
+- Step 4: You can observe the progress on the terminal and Google Drive at the same time
--- PhotoParsingToDrive/main.py
+++ PhotoParsingToDrive/main.py
@@ -8,76 +8,81 @@ from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 
 TEAM_DRIVE_ID = '1PfznI0LCOKGLFHmBEZTefYXP0s-RtV3s'
-PARENT_FOLDER_ID = '1JdvIA2Tto9kxbY7PlW3rOeINOMV3Rzbi'
-
-def create_invalid_images_file(invalid_images: tuple):
-    with os.open('./invalid_images.txt', 'wb') as file:
-        for invalid_image in invalid_images:
-            index, url = invalid_image
-            file.write("Index: {0} -> Invalid URL: {1}\n".format(index, url))
+CATEGORY_FOLDER_IDS = {
+    'Cultural Architecture': '1LMUMeKYOhStB_cC1y4SNkM5xAdy0BzQx',
+    'Healthcare Architecture': '11F39CLte4vlTjlh8P7om1OmeAXOY75CH',
+    'Landscape & Urbanism': '1hSY2nCIhTliTNAvQSBCCTaHiq644Egrj',
+    'Religious Architecture': '139Ljhkbxtv8urmxh19rcuVMmfPaAY7Qi',
+    'Educational Architecture': '1Pjzzpue6biWh9pjOuf3HCdqbhmdW23dN',
+    'Industrial & Infrastructure': '1pHjLLlj9-8zPgRZKmCWOkW3FU2idJIhn',
+    'Hospitality Architecture': '1TkBSgdbmF8QGf8lXktFMnL59n7l_eB_2',
+    'Public Architecture': '1_72IZocWeqzDoq6c79g3VtWV2Z3yBEfI',
+    'Mixed Use Architecture': '1ll5zQ__5IBIAtUAC54rlOXFoG9KMK4_Y',
+    'Sports Architecture': '19qDSG5SIw91aj9Wn3BL_eLQHE5X2__BL',
+    'Residential Architecture': '1tLi1fAvhYK_Q_r3OQ1gWcX_JMN8O71Q3',
+    'Urbanism': '1bDwUZRN0PAqDk4qJWC5wZjzq-nwklEXh',
+}
 
 def setup_google_drive_for_uploading() -> GoogleDrive:
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
     return GoogleDrive(gauth)
 
-def upload_to_google_drive(drive, path: str):
+def upload_to_google_drive(drive, folder: str, path: str):
     file = drive.CreateFile({
         'title': path.split('/')[1],
         'parents': [{
             'kind': 'drive#fileLink',
             'teamDriveId': TEAM_DRIVE_ID,
-            'id': PARENT_FOLDER_ID
+            'id': CATEGORY_FOLDER_IDS[folder]
         }]
     })
+
     file.SetContentFile(path)
     file.Upload(param={'supportsTeamDrives': True})
     file = None
 
 def download_images(image_data: List[tuple]) -> List[tuple]:
-    # Complete the relevant authorization tasks for Google Drive access
-    drive = setup_google_drive_for_uploading()
-
-    invalid_images = []
-    for index, data in enumerate(image_data):
-        print("Started processing image {0}".format(index))
-
-        # Get the url content
-        url, project_id = data
-        content = requests.get(url, allow_redirects=True).content
-
-        # Save the image locally
-        file_path = './{0}_{1}.jpg'.format(project_id, index)
-        open(file_path, 'wb').write(content)
-        
-        try:
-            # Validate image with Image.open which throws an error if not valid
-            Image.open(file_path)
-
-            # Upload the image to Google Drive
-            upload_to_google_drive(drive, file_path)
-
-            print("Finished processing image {0}".format(index))
-        except IOError:
-            invalid_images.append((index, url))
-            print("Failed processing image {0}".format(index))
-        
-        # Delete the local image
-        os.remove(file_path)
-        
-    return invalid_images
+    with open('./invalid_images.txt', 'wb') as invalid_images_file:
+        # Complete the relevant authorization tasks for Google Drive access
+        drive = setup_google_drive_for_uploading()
+    
+        for index, data in enumerate(image_data):
+            print("Started processing image {0}".format(index))
+    
+            # Get the url content
+            url, project_id, project_category = data
+            content = requests.get(url, allow_redirects=True).content
+    
+            # Save the image locally
+            file_path = './{0}_{1}.jpg'.format(project_id, index)
+            open(file_path, 'wb').write(content)
+            
+            try:
+                # Validate image with Image.open which throws an error if not valid
+                Image.open(file_path)
+    
+                # Upload the image to Google Drive
+                upload_to_google_drive(drive, project_category, file_path)
+    
+                print("Finished processing image {0}".format(index))
+            except IOError:
+                invalid_images_file.write("Index: {0} -> Invalid URL: {1}\n".format(index, url))
+                print("Failed processing image {0}".format(index))
+            
+            # Delete the local image
+            os.remove(file_path)
 
 def parse_image_urls(data_path: str) -> List[tuple]:
     data = read_excel(data_path)
 
     image_data = []
     for row in data.index:
-        image_data.append((data['Image_large_size'][row], data['Project_id'][row]))
+        image_data.append((data['Image_large_size'][row], data['Project_id'][row], data['Project_category_0'][row]))
 
     return image_data
 
 if __name__ == '__main__':
     data_path = './data.xlsx'
     image_data = parse_image_urls(data_path)
-    invalid_images = download_images(image_data)
-    create_invalid_images_file(invalid_images)
+    download_images(image_data)
